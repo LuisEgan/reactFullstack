c5c1a5bd9040b0543d0d4ae4f5e17e08bb5ee985
Luis Egan <luisdegan10@hotmail.com>
2017-09-13 00:05:49 -0400

    ayy

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,7 +16,8 @@ class Header extends Component {
             default: //if user is logged in and auth is an object
                 return [
                     <li key="1"><Payments /></li>,
-                    <li key="2"><a href="/api/logout">Logout</a></li>
+                    <li key="2">Current Credits: {this.props.auth.credits}</li>,
+                    <li key="3"><a href="/api/logout">Logout</a></li>
                 ];
         }
     }
